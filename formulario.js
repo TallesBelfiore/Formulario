7148aa652ad8350548c2f17e26f6201fc9efaf39
Talles Belfiore <tallesribeiro111@gmail.com>
2023-03-29 21:35:50 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -18,22 +18,22 @@ form.addEventListener ('submit', (Event)=>{
 function verificacaoDeTexto() {
     if (nome.value.length < 6) {
         console.log("erro")
-        nome.style.border = "1px solid #f44336";
+        nome.style.border = "2px solid #f44336";
         opacidade[0].style.display = "block";
     }else{
         console.log("ok")
-        nome.style.border = "1px solid #3CCC87";
+        nome.style.border = "2px solid #3CCC87";
         opacidade[0].style.display = "none";
     }
 }
 function verificacaoDeTelefone() {
     if (telefone.value.length < 9) {
         console.log("erro")
-        telefone.style.border = "1px solid #f44336";
+        telefone.style.border = "2px solid #f44336";
         opacidade[1].style.display = "block";
     }else{
         console.log("ok")
-        telefone.style.border = "1px solid  #3CCC87";
+        telefone.style.border = "2px solid  #3CCC87";
         opacidade[1].style.display = "none";
     }
 };
@@ -41,11 +41,11 @@ function verificacaoDeTelefone() {
 function verificacaoDeEmail() {
     if (email.value.length < 6) {
         console.log("erro")
-        email.style.border = "1px solid #f44336";
+        email.style.border = "2px solid #f44336";
         opacidade[2].style.display = "block";
     }else{
         console.log("ok")
-        email.style.border = "1px solid  #3CCC87";
+        email.style.border = "2px solid  #3CCC87";
         opacidade[2].style.display = "none";
     }
 };
@@ -53,12 +53,12 @@ function verificacaoDeMensagem() {
     const aviso = document.querySelector(".aviso");
     if (mensagem.value.length < 10) {
         console.log("erro")
-        mensagem.style.border = "1px solid #f44336";
+        mensagem.style.border = "2px solid #f44336";
         opacidade[3].style.display = "block";
         aviso.style.padding = "20px 5px 5px 5px";
     }else{
         console.log("ok")
-        mensagem.style.border = "1px solid #3CCC87";
+        mensagem.style.border = "2px solid #3CCC87";
         opacidade[3].style.display = "none";
         aviso.style.padding = "0px 5px 5px 5px";
     }
